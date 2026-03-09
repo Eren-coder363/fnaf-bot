11da6ed95926744084a04480fa7f6a3910048c54
Eren-coder363 <aa1924932@gmail.com>
2026-03-09 17:47:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,4 @@
 import discord
-from discord.ext import commands
-import os  # Sistemden token çekmek için
-from flask import Flask  # Koyeb'de uyanık kalması için
-from threading import Thread
-
-# --- 1. KOYEB UYANIK TUTMA (KEEP ALIVE) AYARI ---
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot 7/24 Aktif!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-# -----------------------------------------------
-
-# 2. Adım: Intent (Niyet) Ayarları
 intents = discord.Intents.default()
 intents.message_content = True 
 
@@ -30,8 +9,7 @@ bot = commands.Bot(command_prefix=['$'], intents=intents, case_insensitive=True)
 async def on_ready():
     print(f'-----------------------------------')
     print(f'{bot.user} (ID: {bot.user.id}) olarak giriş yaptık!')
-    print(f'Koyeb üzerinden sistem aktif...')
-    print(f'-----------------------------------')
+    print(f'Bot Aktif Edildi')
 
 @bot.command()
 async def hello(ctx):
@@ -63,10 +41,4 @@ William Afton 5 çocuğu öldürdü, Puppet ruhları animatroniklere yerleştird
 """
     await ctx.reply(bolum_1, mention_author=True)
     await ctx.reply(bolum_2, mention_author=True)
-
-# --- ÇALIŞTIRMA ---
-if __name__ == "__main__":
-    keep_alive()  # Koyeb'in botu kapatmasını engeller
-    # Koyeb panelinde tanımlayacağın isimle çekiyoruz:
-    token = os.environ.get('BOT_TOKEN') 
-    bot.run(token)
+bot.run(token)
